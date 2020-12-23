45fdfd5d7db9fe50478ac0bdf6272274a77a5d39
bluesquaredot <62797989+bluesquaredot@users.noreply.github.com>
2020-12-23 17:36:16 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./styles.css";
+import Message from "./Message";
+
+export default function App() {
+  const [toast, setToast] = useState(false);
+  return (
+    <div className="App">
+      <button onClick={() => setToast(!toast)}>Make me a toast</button>
+      {toast ? <Message /> : null}
+    </div>
+  );
+}
---
+++ src/Message.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Message() {
+  return (
+    <section>
+      <h3>Here is your toast</h3>
+    </section>
+  );
+}
+
+export default Message;
